80fb60ccbe8e1adc3690bf2ce7db5358f6a885b9
Henry Rodrigues <108133435+hhennry@users.noreply.github.com>
2023-05-18 21:25:07 -0300

    Delete config/src/module/footer directory

--- config/src/module/footer/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Footer } from './app'
-
-ReactDOM.createRoot(document.getElementById('footer') as HTMLElement).render(
-  <React.StrictMode>
-    <Footer />
-  </React.StrictMode>,
-)
